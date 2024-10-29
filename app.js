f8ffa6dab36a4addb0a079b5ea46b4d4b568aad7
AP7Works <156809673+AP7Works@users.noreply.github.com>
2024-10-29 10:54:25 +0100

    Update app.js

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true })); 
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
